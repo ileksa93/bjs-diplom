d4e3e7afca8fbb1520c03b04e54a8d80ec5c91b1
ileksa <ileksababy@gmail.com>
2023-02-19 01:22:58 +0300

    dfergthyu

---
+++ public/loginPage.js
@@ -0,0 +1,22 @@
+"use strict";
+const userForm = new UserForm();
+userForm.loginFormCallback = function(data) {
+  ApiConnector.login(data, function(response) {
+    if (response.success) {
+      location.reload();
+    } else {
+      userForm.setLoginErrorMessage(response.error);
+    }
+  });
+};
+userForm.registerFormCallback = function(data) {
+    ApiConnector.register(data, function(response) {
+      if (response.success) {
+        location.reload();
+      } else {
+        userForm.setRegisterErrorMessage(response.error);
+      }
+    });
+  };
+  const userForm = new UserForm();
+  
